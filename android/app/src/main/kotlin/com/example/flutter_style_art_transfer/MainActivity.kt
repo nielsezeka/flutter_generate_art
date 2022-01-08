c9b03aa9bf7e18bf6fa7391ca9c2fd476202566a
Na Ngo <ngopona@gmail.com>
2022-01-08 16:44:43 +0700

    init code

---
+++ android/app/src/main/kotlin/com/example/flutter_style_art_transfer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_style_art_transfer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/screens/custom_process_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomProcessScreen extends StatefulWidget {
+  const CustomProcessScreen({Key? key}) : super(key: key);
+
+  @override
+  _CustomProcessScreenState createState() => _CustomProcessScreenState();
+}
+
+class _CustomProcessScreenState extends State<CustomProcessScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
